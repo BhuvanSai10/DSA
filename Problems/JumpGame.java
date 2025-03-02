c1e186b978955f0f8a4144b9d462dac7477fab4c
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-02 23:00:36 +0530

    added new file

---
+++ Problems/JumpGame.java
@@ -0,0 +1,18 @@
+package DSA.Problems;
+
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(canJump(nums));
+    }
+}
