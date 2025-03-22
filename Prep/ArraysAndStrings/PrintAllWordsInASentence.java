2ae91b3d1ab7235964dfecfcee42e2dc981b8d6d
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-22 23:58:19 +0530

    added new file

--- Prep/ArraysAndStrings/PrintAllWordsInASentence.java
+++ Prep/ArraysAndStrings/PrintAllWordsInASentence.java
@@ -1,10 +1,17 @@
 package DSA.Prep.ArraysAndStrings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintAllWordsInASentence {
     public static void printAllWords(String sen){
         int x=0;
         int y=0;
+        char[] chars = sen.toCharArray();
+        List<List<String>> words = new ArrayList<>();
+        for (char ch : chars){
 
+        }
     }
 
     public static void main(String[] args) {
