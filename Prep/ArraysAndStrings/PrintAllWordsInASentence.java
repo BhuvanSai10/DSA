5210b235c847ce45762d93add683a6515909b953
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-23 00:06:29 +0530

    added new file

--- Prep/ArraysAndStrings/PrintAllWordsInASentence.java
+++ Prep/ArraysAndStrings/PrintAllWordsInASentence.java
@@ -5,23 +5,10 @@ import java.util.List;
 
 public class PrintAllWordsInASentence {
     public static void printAllWords(String sen){
-        int x=0;
-        int y=0;
-        char[] chars = sen.toCharArray();
-
-        List<String> words = new ArrayList<>();
-        StringBuilder word = new StringBuilder();
-
-        for (char ch : chars){
-            if (ch!=' '){
-                word.append(ch);
-            }
-            else{
-                words.add(word.toString());
-                word = new StringBuilder();
-            }
+        String[] words = sen.split(" ");
+        for(String word : words){
+            System.out.println(word);
         }
-        System.out.println(words);
     }
 
     public static void main(String[] args) {
