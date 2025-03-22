f731de3b29384d56de2fe1dde1f3bcb41d4bef27
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-22 23:55:52 +0530

    added new file

--- Prep/ArraysAndStrings/PrintAllWordsInASentence.java
+++ Prep/ArraysAndStrings/PrintAllWordsInASentence.java
@@ -1,11 +1,12 @@
 package DSA.Prep.ArraysAndStrings;
 
 public class PrintAllWordsInASentence {
-    public void printAllWords(String sen){
+    public static void printAllWords(String sen){
 
     }
 
     public static void main(String[] args) {
-        String s = "This is a Java Program";
+        String sentence = "This is a Java Program";
+        printAllWords(sentence);
     }
 }
