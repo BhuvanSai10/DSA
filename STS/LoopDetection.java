30170438bbfacf543625f80fc2fe9f6f244acbdc
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-04-27 16:20:51 +0530

    added new file

--- STS/LoopDetection.java
+++ STS/LoopDetection.java
@@ -43,11 +43,25 @@ public class LoopDetection {
 
         return false;
     }
+    public static boolean detectLoopUsingTwoPointers(Node head){
+        if (head==null || head.next == null ){
+            return false;
+        }
+        Node slow = head, fast = head;
+        while (fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         head= new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
-        //head.next.next.next = head;  creates loop
-        System.out.println(detectLoopUsingHashMap(head));
+        //head.next.next.next = head;  //creates loop
+        System.out.println(detectLoopUsingTwoPointers(head));
     }
 }
