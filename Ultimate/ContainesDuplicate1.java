115fc287740e849dda0ce21e17b19854f4c6b50e
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-12 13:49:49 +0530

    added new file

---
+++ Ultimate/ContainesDuplicate1.java
@@ -0,0 +1,21 @@
+package DSA.Ultimate;
+
+import java.util.HashSet;
+
+public class ContainesDuplicate1 {
+    public static boolean checkDuplicates(int[] arr){
+        HashSet<Integer> hs = new HashSet<>();
+        for (int num:arr){
+            if (!hs.contains(num)){
+                hs.add(num);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(checkDuplicates(arr));
+    }
+}
