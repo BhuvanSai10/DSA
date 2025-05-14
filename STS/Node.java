856b32a9963dc224f3aceb4a604d5dff5f4b16f0
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-14 19:37:08 +0530

    added new file

--- STS/Node.java
+++ STS/Node.java
@@ -2,7 +2,7 @@ package DSA.STS;
 
 public class Node {
     int data;
-    Node next;
+    public Node next;
     Node(int data){
         this.data = data;
         this.next = null;
---
+++ Ultimate/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package DSA.Ultimate;
+
+import DSA.STS.Node;
+
+public class ReverseLinkedList {
+    public Node reverseList(Node head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        Node prev = null;
+        Node curr = head;
+        while(curr!=null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
