6fcb1b19a92a0da0cb6a8d53efec57e6c808ea49
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-14 20:16:55 +0530

    added new file

---
+++ Ultimate/PlusOne10.java
@@ -0,0 +1,23 @@
+package DSA.Ultimate;
+
+import java.util.Arrays;
+
+public class PlusOne10 {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{4, 3, 2, 1})));
+    }
+}
