692831436ef6f82235aa72b4cf8ec3129bb884fe
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-14 19:54:55 +0530

    added new file

--- Prep/Tress/TreeNode.java
+++ Prep/Tress/TreeNode.java
@@ -1,7 +1,7 @@
 package DSA.Prep.Tress;
 
 public class TreeNode {
-    int val;
+    public int val;
     public TreeNode left;
     public TreeNode right;
     TreeNode(int val) { this.val = val; }
