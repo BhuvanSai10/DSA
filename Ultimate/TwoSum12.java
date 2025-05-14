3a6dfb3036c872560dbefc28488097d162df80e9
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-14 20:23:55 +0530

    added new file

--- Ultimate/TwoSum12.java
+++ Ultimate/TwoSum12.java
@@ -1,10 +1,11 @@
-package DSA.Prep.ArraysAndStrings;
+package DSA.Ultimate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+public class TwoSum12 {
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
@@ -14,5 +15,9 @@ public class TwoSum {
             map.put(nums[i], i);
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 }
