8c734f41335979a1a246d3acb39828ea5168aa6d
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-24 08:21:31 +0530

    final commit

---
+++ DsaSheet/CheckIfArrayIsSorted.java
@@ -0,0 +1,15 @@
+package DSA.DsaSheet;
+
+public class CheckIfArrayIsSorted {
+    public static boolean isSorted(int[] nums){
+        for (int i = 1 ; i< nums.length ; i++){
+            if (!(nums[i-1]<= nums[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isSorted(new int[]{1,0,3,5}));
+    }
+}
