b57b9ef0c51dd0cbab20776423a9222410319283
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-05-24 14:41:49 +0530

    final commit

---
+++ DsaSheet/LinearSearch7.java
@@ -0,0 +1,15 @@
+package DSA.DsaSheet;
+
+public class LinearSearch7 {
+    public static int search(int[] arr,int key){
+        for (int i = 0 ; i<arr.length ; i++){
+            if (arr[i] == key)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(search(arr,3));
+    }
+}
